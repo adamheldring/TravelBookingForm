8ca04133d83ccbab4383218edbc8447b79e65b09
A-Game <javascript@adamheldring.com>
2018-10-10 00:08:48 +0200

    Extended layout

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,7 +5,27 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <nav>Meny</nav>
+        <header>
+          <div className="header-left-container">
+            <nav>
+              <div className="hamburger-container">
+                <div className="hamburger-bar" />
+                <div className="hamburger-bar" />
+                <div className="hamburger-bar" />
+              </div>
+            </nav>
+            <p>Meny</p>
+          </div>
+          <div className="header-right-container">
+            <p>Sök</p>
+            <p>Inloggning</p>
+            <div className="blue-stripe" />
+            <div className="logo-container">
+              <img src="./images/ident.svg" alt="Norwegian-logo" />
+            </div>
+          </div>
+        </header>
+
         <img className="hero" src="./images/mallorca_1480x500.jpg" alt="Mallorca" />
         <div className="main-wrapper">
           <div className="booking-tabs-container">
@@ -29,7 +49,18 @@ class App extends React.Component {
             <div className="flight-suggestion">
               <p>Stockholm-Arlanda</p>
             </div>
+            <div className="button-container">
+              <button>Sök och beställ</button>
+            </div>
+
           </div>
+          <section className="lower-section-container">
+            <div className="promotion-container">
+              <h1>Erbjudanden just nu</h1>
+              <p>Från/enkel resa inkl. skatter. <span>Visa alla destinationer</span></p>
+              <img src="./images/bigcity.jpg" />
+            </div>
+          </section>
         </div>
       </div>
     )
