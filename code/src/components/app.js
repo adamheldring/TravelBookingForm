53e6dc08e5ce4fa0abb4e4df9136e60e35713ae0
A-Game <javascript@adamheldring.com>
2018-10-09 16:04:25 +0200

    Basic layout

--- code/src/components/app.js
+++ code/src/components/app.js
@@ -5,7 +5,32 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        Find me in src/app.js!
+        <nav>Meny</nav>
+        <img className="hero" src="./images/mallorca_1480x500.jpg" alt="Mallorca" />
+        <div className="main-wrapper">
+          <div className="booking-tabs-container">
+            <ul className="booking-options-list">
+              <li className="booking-tab-active">Flyg</li>
+              <li className="booking-tab">Flyg + Hotell</li>
+              <li className="booking-tab">Hyrbil</li>
+              <li className="booking-tab">Hotell</li>
+              <li className="booking-tab">Reward</li>
+            </ul>
+          </div>
+          <div className="booking-container">
+            <div className="flight-search-container">
+              <h3>Flyg från</h3>
+              <input type="text" />
+            </div>
+            <div className="flight-search-container">
+              <h3>Flyg till</h3>
+              <input type="text" />
+            </div>
+            <div className="flight-suggestion">
+              <p>Stockholm-Arlanda</p>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
